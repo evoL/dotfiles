180d4d7ba8fba53e2e9d5fafb73e5d332490c005
Rafał Hirsz <rafal@hirsz.co>
2023-04-06 23:10:20 +0200

    Remove baked in bookmarks.fish plugin
    Instead, I'll work on maintaining the Fisher plugin list.

--- private_dot_config/private_fish/functions/_bf_csv_unescape.fish
+++
@@ -1,7 +0,0 @@
-# Unescapes a CSV field.
-#
-# $ [ (echo '"hello,"""' | _bf_csv_unescape) = 'hello,"' ]
-# $ [ (echo '"hello"'    | _bf_csv_unescape) = hello ]
-function _bf_csv_unescape
-    sed -E 's/^"(.*)"/\1/' | sed -E 's/""/"/'
-end
--- private_dot_config/private_fish/functions/_bf_list_keys.fish
+++
@@ -1,3 +0,0 @@
-function _bf_list_keys --description 'Lists bookmark keys'
-    cat $argv[1] | string replace --filter --regex '^([^,]*),.*$' '$1'
-end
